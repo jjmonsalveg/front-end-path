2e0fb24c7afdb25f0f918bb53eadd44a084d41a0
Jonathan Monsalve <j.jmonsalveg@gmail.com>
2023-09-21 07:58:24 -0600

    feat(js): inheritance

--- learn_enough_js_tutorial/palindrome.js
+++ learn_enough_js_tutorial/palindrome.js
@@ -3,13 +3,25 @@ function reverse(string) {
   return Array.from(string).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
-}
-
 //Defines a Phrase object
 function Phrase(content) {
   this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  };
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(processedContent);
+  };
 }
+
+//Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+}
+
+TranslatedPhrase.prototype = new Phrase();
