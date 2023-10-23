f407510de2100b9c1967e7c283e88da143589262
Jonathan Monsalve <j.jmonsalveg@gmail.com>
2023-10-23 12:42:00 -0600

    feat(js): Add new letter method to Phrase and refactor palindrome

--- learn_enough_js_tutorial/palindrome/index.js
+++ learn_enough_js_tutorial/palindrome/index.js
@@ -16,12 +16,29 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
   };
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
+  };
+
+  //Returns the letters in the content.
+
+  //For example:
+  //new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    let theLetters = [];
+    const letterRegex = /[a-z]/i;
+
+    Array.from(this.content).forEach(function (character) {
+      if (character.match(letterRegex)) {
+        theLetters.push(character);
+      }
+    });
+
+    return theLetters.join("");
   };
 }
 
--- learn_enough_js_tutorial/palindrome/test/test.js
+++ learn_enough_js_tutorial/palindrome/test/test.js
@@ -18,6 +18,16 @@ describe("Phrase", function () {
       assert(mixedCase.palindrome());
     });
 
-    it("should return true for a palidrome with punctuation");
+    it("should return true for a palidrome with punctuation", function () {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
+  });
+
+  describe("#letters", function () {
+    it("should return only letters", function () {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+    });
   });
 });
